8c3da0cf1908d261510d855f3e02f806fb582d02
joliot <joliot@maike.local>
2020-11-03 21:03:35 +0100

    ajout

--- ProgrammationObjet_chengwanliYANG/src/atelier04/conditionnel3.java
+++ ProgrammationObjet_chengwanliYANG/src/atelier04/conditionnel3.java
@@ -4,14 +4,15 @@ public class conditionnel3 {
 
 	public static void main(String[] args) {
 
-		int p1 = 10, p2 = -1, p3 = 9, p4 = 4, p5 = 8;
-
+		int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8;
+		
 		mediane(p1, p2, p3);
 		mediane(p1, p3, p2);
 		mediane(p2, p1, p3);
 		mediane(p2, p3, p1);
 		mediane(p3, p1, p2);
 		mediane(p3, p2, p1);
+		
 	}
 
 	public static int mediane(int p1, int p2, int p3) {
