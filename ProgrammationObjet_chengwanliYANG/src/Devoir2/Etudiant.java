b2261148502e2638a07006f80072732ec922b61d
joliot <joliot@localhost>
2020-11-11 19:05:13 +0100

    devoir 2

--- ProgrammationObjet_chengwanliYANG/src/Devoir2/Etudiant.java
+++ ProgrammationObjet_chengwanliYANG/src/Devoir2/Etudiant.java
@@ -19,6 +19,6 @@ public class Etudiant {
 		this.prenom = prenom;
 		this.numeroEtudiant = numero;
 		
+	
 	}
-
 }
--- ProgrammationObjet_chengwanliYANG/src/Devoir2/Promotion.java
+++ ProgrammationObjet_chengwanliYANG/src/Devoir2/Promotion.java
@@ -1,10 +1,37 @@
 package Devoir2;
 
+import java.util.*;
+import Devoir2.Etudiant;
+
 public class Promotion {
+	String nom, annee;
 	
-
-	public Promotion() {
-		
+	public Promotion(String nom_promo, String annee_promo) {
+	
+		this.nom=nom_promo;
+		this.annee=annee_promo;
 	}
-
+	
+	public void setNom(String nomPromo) {
+		this.nom=nomPromo;
+	}
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setAnnee(String anneePromo) {	
+		this.annee=anneePromo;
+	}
+	public String getAnnee() {
+		return annee;
+	}
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Nom de promotion：");
+		String nompro = input.next();
+		
+		System.out.print("Annee de promotion：");
+		String anneepro = input.next();
+	}
 }
