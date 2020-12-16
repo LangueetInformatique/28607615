11c6a7ce78666cd181d279e42505e769939c9f8f
joliot <joliot@localhost>
2020-12-16 20:07:12 +0100

    ajout

---
+++ ProgrammationObjet_chengwanliYANG/src/atelier11/Partie2.java
@@ -0,0 +1,29 @@
+package atelier11;
+
+import java.awt.GridLayout;
+
+public class Partie2 {
+	
+	/*
+	 * Les classes de type Layout font partie du package java.awt. 
+	 * Donner la liste des classes de type Layout.
+	 */
+	
+	/* liste des classes java.awt : color, font, list, desktop, alpharcompositf, dnd, event, geom, im, peer, etc.
+	
+	/* Liste des classes de type Layout : 
+	 * Layout 
+	 * FlowLayout 	
+	 * BorderLayout
+	 * GridLayout
+	 */
+
+	/* 
+	 * Créer un plan de la salle D219 sous la forme d’une grille 5x4 en indiquant les emplacements de chaque étudiant. 
+	 * 
+	 * Ajouter sur le référentiel le fichier listeEtudiants contenant vos noms, prénoms et emplacement sur le plan
+	 * 
+	 */
+	GridLayout salle219 = new GridLayout(5, 4);
+	
+}
