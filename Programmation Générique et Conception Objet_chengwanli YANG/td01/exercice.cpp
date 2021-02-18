9ed1e9211133de6e159cd51a725af0957d5496ad
Montacie <Claude.Montacie@Paris-Sorbonne.fr>
2021-02-18 14:19:38 +0100

    td01

---
+++ Programmation Générique et Conception Objet_chengwanli YANG/td01/exercice.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : exercice.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+//danhua
+/*fdf
+ * dsfsd
+ * dsfsdf
+ */
+
+
+#include <iostream>
+using namespace std;
+
+
+
+#define day 7  //宏常量
+
+int main() {
+
+	const int month = 12; //const 修饰变量
+	cout << "一年有" << month << "个月" << endl;
+	return 0;
+}
